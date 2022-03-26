8bcc4cf151e1df2f05b8447ba7e729761f80b3f2
Keshav Sriram <sriramk@usc.edu>
2022-03-26 11:03:01 -0700

    Added text to speech capability'

---
+++ voice.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+class Voice:
+	def __init__(self):
+		self.engine = pyttsx3.init('espeak')            # Initialize using Microsoft Speech API
+		voices = self.engine.getProperty('voices')     # Gets properties of the current voice
+		self.engine.setProperty('voice', voices[0].id)
+
+	def speak(self, audio):
+		self.engine.say(audio)
+		self.engine.runAndWait() # Makes the audio available to the user
+		
+		
+if __name__ == "__main__":
+	fred = Voice()
+	fred.speak('Put that away, idiot!')
