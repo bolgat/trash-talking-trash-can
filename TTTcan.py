f10514ce09c29e920f89e15fe4e131c97526be2f
Jerry Appelhans <appelhan@usc.edu>
2022-03-26 14:21:32 -0700

    Updated state machine

--- TTTcan.py
+++ TTTcan.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import csv
 import pickle
 import time
+import grovepi
 
 # Data storage format
 class TrashData:
@@ -16,6 +17,7 @@ class TrashData:
 class TTTcan:
 	def __init__(self):
 		self.state = "IDLE"
+		self.baseline_trash = self.get_trash_level()
 		self.trash_level = 0
 		self.trash_count = 0
 		self.not_moving_time = 20
@@ -34,7 +36,7 @@ class TTTcan:
 	'''
 	def state_transition(self):
 		if(self.state == "IDLE"):
-			if(self.detect_motion()):
+			'''if(self.detect_motion()):
 				self.state = "MOVING"
 				self.say_voice_line()
 			elif(self.detect_light()):
@@ -42,22 +44,23 @@ class TTTcan:
 				self.say_voice_line()
 			else:
 				self.state = "IDLE"
+			'''
 
-		if(self.state == "OPEN"):
-
-			if(self.detect_motion):
+			'''if(self.detect_motion):
 				self.state = "MOVING"
 				self.say_voice_line()
+				
 			elif(self.detect_darkness):
 				self.state = "IDLE"
 			else:
 				self.state = "OPEN"
-
-			if(self.detect_object):
+			'''
+			if(self.detect_object()):
 				self.trash_count += 1
 				self.say_voice_line()
 				self.log_data()
-
+		
+		'''
 		if(self.state == "MOVING"):
 
 			if(self.not_moving_time == 0):
@@ -69,15 +72,50 @@ class TTTcan:
 			else:
 				self.state = "MOVING"
 				self.not_moving_time = 20
+		'''
 
 	def log_data(self):
-		trash_level = self.get_trash_level()
+		self.trash_level = self.get_trash_level()
 		trash_data.append(TrashData(
-			self.trash_level, 
+			self.trash_level / self.baseline_trash, 
 			self.trash_count
 		))
 		with open("datafile.txt", "wb") as datafile:
 			pickle.dumps(self.data, data_file)
+
+
+	def detect_object(self):
+		ultrasonic_ranger = 7
+		try:
+        	# Read distance value from Ultrasonic
+        	distance = grovepi.ultrasonicRead(ultrasonic_ranger)
+
+ 		except Exception as e:
+        	print ("Error:{}".format(e))
+		
+		if (distance < 30):
+			return True
+		else:
+			return False
+
+
+	def get_trash_level(self):
+		ultrasonic ranger = 8
+		try:
+        	# Read distance value from Ultrasonic
+        	distance = grovepi.ultrasonicRead(ultrasonic_ranger)
+
+ 		except Exception as e:
+        	print ("Error:{}".format(e))
+		
+		return distance
+	
+	def say_voice_line(self):
+		print("object detected")
+
+		
+
+		
 
 if __name__ == "__main__":
 
@@ -86,3 +124,9 @@ if __name__ == "__main__":
 	while True:
 		time.sleep(0.1) # Wait for 1 second between state transition
 		Fred.state_transition()
+		
+
+
+
+
+
