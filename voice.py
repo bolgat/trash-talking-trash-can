c277956d358aa25f51307d95e617111cf3f3d429
Keshav Sriram <sriramk@usc.edu>
2022-03-27 08:42:35 -0700

    created voice lines

--- voice.py
+++ voice.py
@@ -4,12 +4,17 @@ class Voice:
 	def __init__(self):
 		self.engine = pyttsx3.init('espeak')            # Initialize using Microsoft Speech API
 		voices = self.engine.getProperty('voices')     # Gets properties of the current voice
-		self.engine.setProperty('voice', voices[0].id)
+		self.engine.setProperty('voice', voices[2].id)
 
 	def speak(self, audio):
 		self.engine.say(audio)
 		self.engine.runAndWait() # Makes the audio available to the user
 		
+	def save_audio(self, audio, audiofile):
+		self.engine.save_to_file(audio, audiofile)
+		self.engine.runAndWait()
+		
+		
 if __name__ == "__main__":
 	fred = Voice()
-	fred.speak('Put that away, idiot!')
+	fred.save_audio('I\'m not meant to be airborne...', './audio/1.mp3')
